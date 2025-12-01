1751bc601ad246700591d6d65cf573054acb1eec
Jose Cabrera <joserobe167@gmail.com>
2025-11-30 23:19:38 -0600

    Push Final

--- pokedex_JoseCabrera/vite.config.js
+++ pokedex_JoseCabrera/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/pokedex/",
+  base: "/pokedex1/",
   plugins: [
     tailwindcss(),
     react()
